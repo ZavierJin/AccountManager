de87c519713586bbc0e88b5ad6c4f1f3e54a1bc7
czk <7674647+czk110@user.noreply.gitee.com>
2020-06-16 15:39:18 +0800

    删除文件 src

--- src/Request/BalanceCheck.cpp
+++
@@ -1,9 +0,0 @@
-// BalanceCheck.cpp
-// Brief introduction: the implementation of BalanceCheck's method
-// Create by Zhang Zhecheng 2020/5/18
-#include "Request.h"
-namespace request
-{
-    BalanceCheck::BalanceCheck(const string &username)
-        : UserRequest(username, Kind::BalanceCheck){}
-} // namespace request
